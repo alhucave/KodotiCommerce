26be059d3aafb1e1f8398c3672fe60849028231a
Alex Castrillon <alex.castrillon@sietesoluciones.com>
2022-09-26 16:14:55 -0500

    Agregar archivos de proyecto.

---
+++ src/Services/Catalog/Catalog.Service.EventHandlers/ProductInStockUpdateStockEventHandler.cs
@@ -0,0 +1,95 @@
+﻿using Catalog.Domain;
+using Catalog.Persistence.Database;
+using Catalog.Service.EventHandlers.Commands;
+using Catalog.Service.EventHandlers.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static Catalog.Common.Enums;
+
+namespace Catalog.Service.EventHandlers
+{
+    public class ProductInStockUpdateStockEventHandler :
+        INotificationHandler<ProductInStockUpdateStockCommand>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ProductInStockUpdateStockEventHandler> _logger;
+        private static bool _falla = false;
+        private static readonly Random Random = new Random();
+
+        public ProductInStockUpdateStockEventHandler(
+            ApplicationDbContext context,
+            ILogger<ProductInStockUpdateStockEventHandler> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task Handle(ProductInStockUpdateStockCommand notification, CancellationToken cancellationToken)
+        {
+            //Código para generar fallas aleatorias 
+            var ran = Random.Next(1, 2);
+            _logger.LogInformation($"--- Ramdom" + ran.ToString());
+            if (ran == 1)
+            {
+                _falla = true;
+            }
+
+            if (_falla)
+            {
+                _falla = false;
+                throw new Exception("Courrio un error");
+
+            }
+
+
+            //_logger.LogInformation("--- ProductInStockUpdateStockCommand started");
+
+            var products = notification.Items.Select(x => x.ProductId);
+            var stocks = await _context.Stocks.Where(x => products.Contains(x.ProductId)).ToListAsync();
+
+            //_logger.LogInformation("--- Retrieve products from database");
+
+            foreach (var item in notification.Items) 
+            {
+                var entry = stocks.SingleOrDefault(x => x.ProductId == item.ProductId);
+
+                if (item.Action == ProductInStockAction.Substract)
+                {
+                    if (entry == null || item.Stock > entry.Stock)
+                    {
+                        _logger.LogError($"--- Product {entry.ProductId} -doens't have enough stock");
+                        throw new ProductInStockUpdateStockCommandException($"Product {entry.ProductId} - doens't have enough stock");
+                    }
+
+                    entry.Stock -= item.Stock;
+
+                    _logger.LogInformation($"--- Product {entry.ProductId} - its stock was subtracted and its new stock is {entry.Stock}");
+                }
+                else
+                {
+                    if (entry == null)
+                    {
+                        entry = new ProductInStock
+                        {
+                            ProductId = item.ProductId
+                        };
+
+                        _logger.LogInformation($"--- New stock record was created for {entry.ProductId} because didn't exists before");
+
+                        await _context.AddAsync(entry);
+                    }
+
+                    _logger.LogInformation($"--- Add stock to product {entry.ProductId}");
+                    entry.Stock += item.Stock;
+                }
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Services/Order/Order.Service.Proxies/Catalog/CatalogProxy.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Order.Service.Proxies.Catalog.Commands;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Order.Service.Proxies.Catalog
+{
+    public interface ICatalogProxy
+    {
+        Task UpdateStockAsync(ProductInStockUpdateStockCommand command);
+    }
+
+    public class CatalogProxy : ICatalogProxy
+    {
+        private readonly ApiUrls _apiUrls;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+
+        private static readonly Random Jitterer = new Random();
+        private static readonly AsyncRetryPolicy<HttpResponseMessage> TransientErrorRetryPolicy =
+            Policy.HandleResult<HttpResponseMessage>(
+                message => ((int)message.StatusCode) == 429 || (int)message.StatusCode >= 500)
+            .WaitAndRetryAsync(2, sleepDurationProvider: retryAttemp =>
+            {
+                Console.WriteLine($"Reintentando: {retryAttemp}");
+                return TimeSpan.FromSeconds(Math.Pow(2, retryAttemp)) + TimeSpan.FromMilliseconds(Jitterer.Next(0, 1000));
+            });
+
+
+        private static readonly AsyncCircuitBreakerPolicy<HttpResponseMessage> CircuitBreakerPolicy =
+            Policy.HandleResult<HttpResponseMessage>(message => ((int)message.StatusCode) == 429 || (int)message.StatusCode >= 500)
+            .CircuitBreakerAsync(2, TimeSpan.FromSeconds(15));
+            
+        public CatalogProxy(
+            IHttpClientFactory httpClientFactory,
+            IOptions<ApiUrls> apiUrls,
+            IHttpContextAccessor httpContextAccessor)
+        {
+
+
+            _httpClientFactory = httpClientFactory;
+            _apiUrls = apiUrls.Value;
+        }
+
+        public async Task UpdateStockAsync(ProductInStockUpdateStockCommand command)
+        {
+            if (CircuitBreakerPolicy.CircuitState == CircuitState.Open)
+            {
+                throw new Exception("Circuito abierto");
+            }
+
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(command),
+                Encoding.UTF8,
+                "application/json"
+            );
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await CircuitBreakerPolicy.ExecuteAsync(() =>
+
+                    TransientErrorRetryPolicy.ExecuteAsync(() =>
+                    httpClient.PutAsync(_apiUrls.CatalogUrl + "v1/stocks", content))
+                );
+
+
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Fallo UpdateStockAsync");
+            }
+
+        }
+    }
+}
